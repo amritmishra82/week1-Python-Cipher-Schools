2a29ea3d5ba8e091d20422aa85d4f2a4d299fff4
amritmishra82 <115808810+amritmishra82@users.noreply.github.com>
2022-12-01 23:47:15 +0530

    Add files via upload

---
+++ Class3-Print command functionalities-CipherSchools.py
@@ -0,0 +1,17 @@
+#knowing more about print
+
+print("Jatin Katyal")
+print(1)
+
+
+#print can accept multiple values at once
+print(1, 2, 3, "Jatin", 3.4, 1 + 3j)
+
+
+print("hello", end=";")
+print("world")
+
+
+
+print(1, 2, 3, "Jatin", 3.4, 1 + 3j, sep=",")
+
---
+++ Class4-Taking user input and typecasting-CipherSchools.py
@@ -0,0 +1,15 @@
+# taking input from user
+a = input()
+b = input()
+print(a)
+print(b)
+print(type(a))
+print(type(b))
+
+
+#typecasting
+c = 15
+print(type(c))
+c = str(c)
+print(type(c))
+
